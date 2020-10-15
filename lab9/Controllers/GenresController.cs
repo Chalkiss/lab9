241c4d5483ce3216108bef0bdc7f0b9263879c0f
FGap <filgap@wp.pl>
2020-10-15 15:57:23 +0200

    Dodaj pliki projektów.

---
+++ lab9/Controllers/GenresController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using lab9.Models;
+
+namespace lab9.Controllers
+{
+    public class GenresController : Controller
+    {
+        private MusicDbContext db = new MusicDbContext();
+
+        // GET: Genres
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Genres.ToListAsync());
+        }
+
+        // GET: Genres/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = await db.Genres.FindAsync(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        // GET: Genres/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Genres/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "GenreId,Name")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Genres.Add(genre);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(genre);
+        }
+
+        // GET: Genres/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = await db.Genres.FindAsync(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        // POST: Genres/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "GenreId,Name")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(genre).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(genre);
+        }
+
+        // GET: Genres/Delete/5
+
+
+        // POST: Genres/Delete/5
+        
+        public async Task<ActionResult> Delete(int id)
+        {
+            Genre genre = await db.Genres.FindAsync(id);
+            db.Genres.Remove(genre);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ lab9/Models/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab9.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public int GenreId { get; set; }
+    }
+}
